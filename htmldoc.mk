34b5ba0108a6e68b81ea5c09f0db6815da22f4fa
Marcus Mennemeier <marcus@mennemeier.de>
2014-06-20 13:32:07 +0200

    touchfile for node_modules, added realclean target to clean node_modules

--- htmldoc.mk
+++ htmldoc.mk
@@ -5,10 +5,14 @@ htmldoc: $(HTMLDOC)/htmldoc.tgz
 htmldoc/clean:
 	rm -rf $(HTMLDOC)
 
-$(TOOLS)/htmldoc/node_modules: $(TOOLS)/htmldoc/package.json
-	cd $(TOOLS)/htmldoc && npm install && touch $(TOOLS)/htmldoc/node_modules
+htmldoc/realclean: htmldoc/clean
+	rm -rf $(TOOLS)/htmldoc/node_modules
 
-$(HTMLDOC)/index.html: $(LOCAL_COMPONENTS)/lib/htmldoc/component-build/component-is-build $(TOOLS)/htmldoc/node_modules $(TOOLS)/htmldoc/htmldoc.jade
+$(TOOLS)/htmldoc/node_modules/package.json.d: $(TOOLS)/htmldoc/package.json
+	cd @D/.. && npm install
+	@touch $@
+
+$(HTMLDOC)/index.html: $(LOCAL_COMPONENTS)/lib/htmldoc/component-build/component-is-build $(TOOLS)/htmldoc/node_modules/package.json.d $(TOOLS)/htmldoc/htmldoc.jade
 	@rm -rf $(HTMLDOC)
 	@mkdir -p "$(HTMLDOC)"
 	tar -c --exclude component-is-build --directory "$(<D)" . | tar -x --directory "$(HTMLDOC)"
@@ -21,3 +25,8 @@ $(HTMLDOC)/htmldoc.tgz: $(HTMLDOC)/index.html
 	@rm -f $(HTMLDOC)/htmldoc.tgz
 	cd $(HTMLDOC) && tar -czf htmldoc.tgz --exclude htmldoc.tgz *
 
+.PHONY: htmldoc htmldoc/clean htmldoc/realclean
+
+install: htmldoc
+mostlyclean: htmldoc/clean
+realclean: htmldoc/realclean
