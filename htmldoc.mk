b7e3f1f225237317ba4a57fa4d8b61e6f53811b9
Andreas Lubbe <git@lubbe.org>
2015-04-08 10:43:13 +0200

    fixed npm running in the wrong working directory

--- htmldoc.mk
+++ htmldoc.mk
@@ -7,7 +7,7 @@ $(HTMLDOC)/index.html: $(LOCAL_COMPONENTS)/lib/htmldoc/component-build/component
 	@rm -rf $(HTMLDOC)
 	@mkdir -p "$(HTMLDOC)"
 	tar -c --exclude component-is-build --directory "$(<D)" . | tar -x --directory "$(HTMLDOC)"
-	npm run coffee -- $(HTMLDOC_SRC)/htmldoc.coffee
+	cd $(HTMLDOC_SRC) && npm run coffee -- htmldoc.coffee
 	@touch $(HTMLDOC)/index.html
 
 .SECONDARY: $(HTMLDOC)/%
