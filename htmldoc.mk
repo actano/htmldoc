947e3c30163fc3eb63cad755af5ab3320803d752
Marcus Mennemeier <m.mennemeier@global-communication.de>
2015-05-08 18:26:33 +0200

    drop npm script coffee, upgrade to coffee 1.9.2

--- htmldoc.mk
+++ htmldoc.mk
@@ -1,13 +1,14 @@
 HTMLDOC := $(BUILD)/htmldoc
 HTMLDOC_SRC := $(dir $(lastword $(MAKEFILE_LIST)))
-
+COFFEE_CLI=$(shell node -e 'path = require("path"); p = require.resolve("coffee-script"); while (p && path.basename(path.dirname(p)) != "node_modules") p = path.dirname(p); p = path.join(p, "bin", "coffee"); console.log(p)')
+$(info $(NPM_BIN))
 htmldoc: $(HTMLDOC)/htmldoc.tgz
 
 $(HTMLDOC)/index.html: $(LOCAL_COMPONENTS)/lib/htmldoc/component-build/component-is-build $(HTMLDOC_SRC)/htmldoc.jade
 	@rm -rf $(HTMLDOC)
 	@mkdir -p "$(HTMLDOC)"
 	tar -c --exclude component-is-build --directory "$(<D)" . | tar -x --directory "$(HTMLDOC)"
-	npm run coffee -- $(HTMLDOC_SRC)/htmldoc.coffee
+	$(COFFEE_CLI) $(HTMLDOC_SRC)htmldoc.coffee
 	@touch $(HTMLDOC)/index.html
 
 .SECONDARY: $(HTMLDOC)/%
